49bd1243bb2ba5891f7de64e03b79c8204e5be96
unknown <joakimdeak@hotmail.com>
2019-12-10 12:02:49 +0100

    still buggy af but need to change pc

--- src/day5/Part1.java
+++ src/day5/Part1.java
@@ -5,7 +5,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Part1 {
-	
+
 	/*
 	 * needs to implement support for immediate mode
 	 */
@@ -13,7 +13,7 @@ public class Part1 {
 	public static void main(String[] args) throws FileNotFoundException {
 		int[] instructions = getInput();
 		instructions = compute(instructions);
-		for(int i : instructions) {
+		for (int i : instructions) {
 			System.out.println(i);
 		}
 	}
@@ -33,13 +33,52 @@ public class Part1 {
 	}
 
 	public static int[] compute(int[] insList) {
-
 		int i = 0;
-		while (insList[i] != 99) {
-			int param1 = insList[insList[i + 1]];
-			int param2 = insList[insList[i + 2]];
-			int param3 = insList[i + 3];
+		while (insList[i] != 99 || i < insList.length) {
 			int cIns = insList[i];
+
+			int modeParam1 = 0;
+			int modeParam2 = 0;
+			String cInsString = String.valueOf(cIns);
+			if (cInsString.length() > 1) {
+				if (cInsString.length() == 4) {
+					modeParam1 = Character.getNumericValue(cInsString.charAt(1));
+					modeParam2 = Character.getNumericValue(cInsString.charAt(0));
+				} else if (cInsString.length() == 3) {
+					modeParam1 = Character.getNumericValue(cInsString.charAt(0));
+				}
+				cIns = cIns % 10;
+			}
+
+			System.out.println(modeParam1);
+			System.out.println(modeParam2);
+
+			int param1 = 0;
+			int param2 = 0;
+			int param3 = 0;
+
+			if (modeParam1 == 0) {
+				param1 = insList[insList[i + 1]];
+			} else if (modeParam1 == 1) {
+				param1 = insList[i + 1];
+			}
+			for (int j = 0; j < 20; j++) {
+				System.out.print(insList[j]);
+				if (j == i) {
+					System.out.print("!");
+				}
+				System.out.print(", ");
+			}
+			System.out.println();
+			if (cIns != 3 && cIns != 4) {
+				if (modeParam2 == 0) {
+					param2 = insList[insList[i + 2]];
+				} else if (modeParam2 == 1) {
+					param2 = insList[i + 2];
+				}
+				param3 = insList[i + 3];
+			}
+
 			switch (cIns) {
 			case 1:
 				insList[param3] = param1 + param2;
@@ -52,13 +91,14 @@ public class Part1 {
 			case 3:
 				Scanner sc = new Scanner(System.in);
 				param1 = insList[i + 1];
-				insList[param1] = sc.nextInt();
+				insList[param1] = 1; // sc.nextInt();
 				sc.close();
 				i += 2;
 				break;
 			case 4:
 				param1 = insList[i + 1];
 				System.out.println(insList[param1]);
+				i += 2;
 				break;
 			}
 		}
